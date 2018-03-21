15f775b03add8c81ca0cdaad951885257e1f08a1
liuyang <541248818@qq.com>
2018-03-21 20:54:19 +0800

    fmt

--- binarytree/binarysearch.go
+++ binarytree/binarysearch.go
@@ -1,4 +1,3 @@
-
 package mybinarysearch
 
 // 二分查找：有序 数组arr中,查找target
@@ -7,41 +6,41 @@ package mybinarysearch
 // O(logn)
 func BinarySearch(arr []string, target string) (bool, int) {
 	// arr[l...r]中查找target
-	l,r := 0, len(arr)-1
+	l, r := 0, len(arr)-1
 	for l <= r {
 		//mid := (l + r)/2 // 两个int相加若超出int最大值溢出
 		mid := l + (r-l)/2
-		
+
 		if arr[mid] == target {
-			return true,mid
+			return true, mid
 		}
-		if arr[mid] > target { 
+		if arr[mid] > target {
 			// arr[l...mid-1]
 			r = mid - 1
-		} else { 
+		} else {
 			// arr[mid+1...r]
 			l = mid + 1
 		}
 	}
-	return false,-1
+	return false, -1
 }
 
 // 递归二分查找
-func BinarySearchRecursion(arr []string, target string) (bool,int) {
+func BinarySearchRecursion(arr []string, target string) (bool, int) {
 	return binarySearchRecursion(arr, 0, len(arr)-1, target)
 }
 
-func binarySearchRecursion(arr []string, l , r int ,target string) (bool,int) {
-	if l>r {
-		return false,-1
+func binarySearchRecursion(arr []string, l, r int, target string) (bool, int) {
+	if l > r {
+		return false, -1
 	}
-	mid := (l + r)/2
+	mid := (l + r) / 2
 	if arr[mid] == target {
-		return true,mid
+		return true, mid
 	} else if arr[mid] > target {
-		return binarySearchRecursion(arr,0,mid-1,target)
+		return binarySearchRecursion(arr, 0, mid-1, target)
 	} else {
-		return binarySearchRecursion(arr,mid+1,r,target)
+		return binarySearchRecursion(arr, mid+1, r, target)
 	}
 }
 
@@ -68,4 +67,3 @@ func FBinarySearch(arr []string, target string) (bool, int) {
 // 普通数组	  O(n)		 O(n)		 O(n)
 // 顺序数组  O(logn)	 O(n)		 O(n)
 // 二分搜索树O(logn)    O(logn)     O(logn)
-
--- binarytree/binaryseatch_test.go
+++ binarytree/binaryseatch_test.go
@@ -1,39 +1,39 @@
 package mybinarysearch
 
-import(
-	"testing"
+import (
 	"fmt"
+	"testing"
 )
 
 func TestBinarySearch(t *testing.T) {
 	fmt.Println("======test BinarySearch")
-	s := []string{"a","b","c","d","e","gg"}
-	exist,index := BinarySearch(s,"d")
+	s := []string{"a", "b", "c", "d", "e", "gg"}
+	exist, index := BinarySearch(s, "d")
 	if exist {
-		fmt.Println("exist,index is:",index)
-	}else {
+		fmt.Println("exist,index is:", index)
+	} else {
 		fmt.Println("not exist")
 	}
 }
 
 func TestBinarySearchRecursion(t *testing.T) {
 	fmt.Println("======test BinarySearchRecursion")
-	s := []string{"a","b","c","d","e","gg"}
-	exist,index := BinarySearchRecursion(s,"d")
+	s := []string{"a", "b", "c", "d", "e", "gg"}
+	exist, index := BinarySearchRecursion(s, "d")
 	if exist {
-		fmt.Println("exist,index is:",index)
-	}else {
+		fmt.Println("exist,index is:", index)
+	} else {
 		fmt.Println("not exist")
 	}
 }
 
 func TestFBinarySearch(t *testing.T) {
 	fmt.Println("======test FBinarySearchs")
-	s := []string{"a","b","c","d","e","gg"}
-	exist,index := FBinarySearch(s,"z")
+	s := []string{"a", "b", "c", "d", "e", "gg"}
+	exist, index := FBinarySearch(s, "z")
 	if exist {
-		fmt.Println("exist,index is:",index)
-	}else {
+		fmt.Println("exist,index is:", index)
+	} else {
 		fmt.Println("not exist")
 	}
 }
