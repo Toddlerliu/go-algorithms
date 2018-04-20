66debbf81201e088dfbcb4c2671064811b2b3e62
SKY-20161121TMF\Administrator <541248818@qq.com>
2018-04-20 17:31:00 +0800

    test listmap

--- map/listmap.go
+++ map/listmap.go
@@ -5,6 +5,7 @@ type Pair struct {
 	Value interface{}
 }
 
+// 有序map(插入顺序)
 type ListMap struct {
 	keys  []interface{}
 	pairs map[interface{}]*Pair
@@ -29,6 +30,7 @@ func (m *ListMap) Contain(k interface{}) bool {
 	return ok
 }
 
+// 如果key存在，更新；不存在，新增；插入顺序
 func (m *ListMap) Add(k, v interface{}) {
 	if value, ok := m.pairs[k]; ok {
 		value.Value = v
@@ -51,4 +53,55 @@ func (m *ListMap) GetTop() (*Pair, bool) {
 		return v, ok
 	}
 	return nil, false
+}
+
+func (m *ListMap) Keys() []interface{} {
+	return m.keys
+}
+
+func (m *ListMap) Values() []interface{} {
+	res := make([]interface{}, 0)
+	for _, v := range m.keys {
+		if value, ok := m.pairs[v]; ok {
+			res = append(res, value.Value)
+		}
+	}
+	return res
+}
+
+func (m *ListMap) Pairs() map[interface{}]*Pair {
+	return m.pairs
+}
+
+func (m *ListMap) Remove(k interface{}) bool {
+	if m.Contain(k) {
+		delete(m.pairs, k)
+		for i, v := range m.keys {
+			if v == k {
+				m.keys = append(m.keys[:i], m.keys[i+1:]...)
+				break
+			}
+		}
+	}
+	return false
+}
+
+func (m *ListMap) RemoveTop() bool {
+	if len(m.keys) > 0 {
+		return m.Remove(m.keys[0])
+	}
+	return false
+}
+
+func (m *ListMap) Iteraror() <-chan *Pair {
+	ch := make(chan *Pair, len(m.keys))
+	go func() {
+		defer close(ch)
+		for _, v := range m.keys {
+			if value, ok := m.pairs[v]; ok {
+				ch <- value
+			}
+		}
+	}()
+	return ch
 }
--- map/listmap_test.go
+++ map/listmap_test.go
@@ -1,4 +1,33 @@
-package main
+package mmap
 
-func main() {
+import (
+	"fmt"
+	"testing"
+)
+
+func TestListMap(t *testing.T) {
+	lm := NewListMap()
+	fmt.Println("Size:", lm.Size())
+	fmt.Println("Inserting...")
+	lm.Add(1, 1)
+	lm.Add(2, 2)
+	lm.Add("a", "y")
+	lm.Add("b", "b")
+	lm.Add("b", "x")
+	fmt.Println("Size:", lm.Size())
+	fmt.Println("Contains(1)", lm.Contain(1))
+	fmt.Println("Contains(c)", lm.Contain("c"))
+	v, ok := lm.Get("b")
+	fmt.Println("Get(b)", v, ok)
+	v1, ok1 := lm.GetTop()
+	fmt.Println("GetTop:", v1, ok1)
+	fmt.Println("Keys:", lm.Keys())
+	fmt.Println("Values:", lm.Values())
+	fmt.Println("Pairs:", lm.Pairs())
+	lm.Remove("b")
+	lm.RemoveTop()
+	fmt.Println("Keys:", lm.Keys())
+	fmt.Println("Values:", lm.Values())
+	vv := lm.Iteraror()
+	fmt.Println("Pairs:", (<-vv).Key, (<-vv).Value)
 }
