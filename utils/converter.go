48976ae705129d1f16b00feb7e4d73e01674120e
SKY-20161121TMF\Administrator <541248818@qq.com>
2018-10-31 16:55:29 +0800

    StringToBytes and BytesToString

--- utils/converter.go
+++ utils/converter.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"math"
 	"encoding/binary"
+	"unsafe"
 )
 
 func Float64ToBytes(float float64) []byte {
@@ -15,4 +16,34 @@ func Float64ToBytes(float float64) []byte {
 func BytesToFloat64(bytes []byte) float64 {
 	bits := binary.BigEndian.Uint64(bytes)
 	return math.Float64frombits(bits)
+}
+
+// String转Bytes
+// runtime
+//func slicebytetostring(b Slice) (s String) {
+//	void *pc;
+//	if(raceenabled) {
+//		pc = runtime.getcallerpc(&b);
+//		runtime.racereadrangepc(b.array, b.len, pc, runtime.slicebytetostring);
+//	}
+//	s = gostringsize(b.len);
+//	runtime.memmove(s.str, b.array, s.len);
+//}
+//
+//func stringtoslicebyte(s String) (b Slice) {
+//	b.array = runtime.mallocgc(s.len, 0, FlagNoScan|FlagNoZero);
+//	b.len = s.len;
+//	b.cap = s.len;
+//	runtime.memmove(b.array, s.str, s.len);
+//}
+// 无论从[]byte到string还是string到[]byte，他们的指针地址均不同。
+// 在类型转换的时候，发生了值拷贝，而[]byte与string不共享内存
+func StringToBytes(s string) []byte {
+	x := (*[2]uintptr)(unsafe.Pointer(&s))
+	h := [3]uintptr{x[0], x[1], x[1]}
+	return *(*[]byte)(unsafe.Pointer(&h))
+}
+
+func BytesToString(byte []byte) string {
+	return *(*string)(unsafe.Pointer(&byte))
 }
